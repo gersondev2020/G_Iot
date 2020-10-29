9c8e266c341b091ce2673e4101c1eeeac3094f67
G_Automation <gersondev2020@gmail.com>
2020-10-29 11:19:05 -0300

    Funcionalidades básica finalizadas, falta login e Splash

--- app/src/main/java/com/gautomation/giot/Class/UnidadesAut.java
+++ app/src/main/java/com/gautomation/giot/Class/UnidadesAut.java
@@ -3,11 +3,13 @@ package com.gautomation.giot.Class;
 public class UnidadesAut {
         public String Valor_padrao_unidade(String tag){
         if( tag.contains("PT") ||  tag.contains("KG")){
-            return "Kgf/m²";
+            return "Kgf/cm²";
         }else if(tag.contains("TT") || tag.contains("TEMPER")){
             return "°C";
-        }else if(tag.contains("FT") || tag.contains("VZ") || tag.contains("VAZAO")){
+        }else if((tag.contains("FT") || tag.contains("VZ") || tag.contains("VAZAO")) && !tag.contains("CAL")){
             return "m³/h";
+        }else if(tag.contains("CAL") && (tag.contains("VZ") || tag.contains("VAZAO") || tag.contains("FT"))){
+            return "tn/h";
         }else if(tag.contains("RPM")){
             return "RPM";
         }else if(tag.contains("PH") || tag.contains("NIVEL") || tag.contains("LT")){
@@ -15,7 +17,7 @@ public class UnidadesAut {
         }else if(tag.contains("POT") || tag.contains("EXPO")){
             return "KW";
         }else{
-            return "Kgf/m²";
+            return "Kgf/cm²";
         }
 
     }
